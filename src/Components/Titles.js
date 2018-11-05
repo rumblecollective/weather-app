31b0603ba151f3106e4a298bdd82836e3ad73c63
tonyynot <aramella@protonmail.com>
2018-11-04 23:53:57 -0500

    add styling to application

--- src/Components/Titles.js
+++ src/Components/Titles.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Titles = props => {
     return(
         <div>
-            <h1>Weather Finder</h1>
+            <h1 className="title-container__title">Weather Finder</h1>
             <p>Find out temperature, conditions and more...</p>
         </div>
     );
